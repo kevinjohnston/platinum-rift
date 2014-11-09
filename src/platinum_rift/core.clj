ac2cc3c7d313912ab7039d773d981bb59ffd3cdd
Kevin Lee Johnston <kevinjohnston512@gmail.com>
2014-11-09 17:53:44 -0600

    Created basic strategy

--- src/platinum_rift/core.clj
+++ src/platinum_rift/core.clj
@@ -1,7 +1,18 @@
 (ns platinum-rift.core
+  (:require [platinum-rift.graph :as graph]
+            [platinum-rift.strategies :as strat]
+            [platinum-rift.world :as world])
   (:gen-class))
+
+(def debug true)
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
+  ;;create a world
+  (world/reset-world)
+  ;;pick a strategy
+  (let [strat (strat/pick-strat)]
+  (if debug (name strat)))
+  ;;pick tactics
   (println "Hello, World!"))
---
+++ src/platinum_rift/tactics.clj
@@ -0,0 +1,1 @@
+(ns platinum-rift.tactics)
